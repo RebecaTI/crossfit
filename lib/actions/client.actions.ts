d402004a2a2358fc7dd6824332cb533959368296
RebecaTI <mebuffbeka@gmail.com>
2025-05-28 15:42:28 -0300

    Add Appwrite SDK integration and user-related types; update appwrite.config and package files

---
+++ lib/actions/client.actions.ts
@@ -0,0 +1,29 @@
+ 
+
+declare type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+declare type Gender = "male" | "female" | "other";
+
+declare interface CreateUserParams {
+  name: string;
+  email: string;
+  phone: string;
+}
+declare interface User extends CreateUserParams {
+  $id: string;
+}
+
+declare interface RegisterUserParams extends CreateUserParams {
+  userId: string;
+  birthDate: Date;
+  gender: Gender;
+  address: string;
+  emergencyContactName: string;
+  emergencyContactNumber: string;
+  currentMedication: string | undefined;
+  pastMedicalHistory: string | undefined;
+  privacyConsent: boolean;
+}
