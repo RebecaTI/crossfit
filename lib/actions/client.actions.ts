c0e8452449d7169dc216394f35c47d13c2e78a71
RebecaTI <mebuffbeka@gmail.com>
2025-06-26 15:28:43 -0300

    refactor: comment out user creation logic in createUser function for future implementation

--- lib/actions/client.actions.ts
+++ lib/actions/client.actions.ts
@@ -1,17 +1,17 @@
 'use server'
 
-import { ID, Query } from 'node-appwrite'
+import { Query } from 'node-appwrite'
 import { users } from '../appwrite.config'
 
 export const createUser = async (user: CreateUserParams) => {
   try {
-    const newUser = await users.create(
-      ID.unique(),
-      user.email,
-      user.phone,
-      undefined,
-      user.name
-    )
+    // const newUser = await users.create(
+    //   ID.unique(),
+    //   user.email,
+    //   user.phone,
+    //   undefined,
+    //   user.name
+    // )
   } catch (error: any) {
     if (error && error?.code === 409) {
       const existingUser = await users.list([
