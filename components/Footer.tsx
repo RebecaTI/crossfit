3fa1db992fe287a24407e59510d4d21f79aa83bc
RebecaTI <mebuffbeka@gmail.com>
2025-06-26 13:51:10 -0300

    refactor: update Footer layout and styling for improved responsiveness and consistency; replace twitter.svg with corrected version

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className="flex flex-col pt-10 align-items-center px-10 text-gray-400">
-      <div className="flex flex-col">
+    <section className="flex flex-col py-10 align-items-center px-10 text-gray-400">
+      <div className="flex flex-col sm:flex-row sm:gap-5">
         <Image
           src="kropp-footer-logo.svg"
           alt="Kropp Logo"
@@ -12,17 +12,18 @@ const Footer = () => {
           height={100}
           className=" mx-auto mb-4"
         />
-        <h2 className="text-center">please feel free to send us an e-mail at  For any additional inquiries</h2>
+        <h2 className="text-center font-thin">please feel free to send us an e-mail at  For any additional inquiries</h2>
       </div>
-      <div className="pt-10">
+
+      <div className="pt-10 sm:grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
 
         <div className=" pb-5">
-          <h1 className="text-center font-bold text-white font-mono">ABOUT</h1>
-          <p>Shape up yur site with Kropp, a theme especially made for fitness & gym websites.</p>
+          <h1 className="text-center font-bold text-white font-mono md:text-start">ABOUT</h1>
+          <p className="">Shape up yur site with Kropp, a theme especially made for fitness & gym websites.</p>
         </div>
 
-        <div className="py-5">
-          <h1 className="text-center font-bold text-white font-mono">WORKING HOURS</h1>
+        <div className="py-5 sm:pt-0">
+          <h1 className="text-center font-bold text-white font-mono md:text-start">WORKING HOURS</h1>
           <ul className="flex flex-col gap-3">
             <li>
               <h2>Monday - Friday</h2>
@@ -36,15 +37,17 @@ const Footer = () => {
         </div>
 
         <div className="py-5">
-          <h1 className="text-center font-bold text-white font-mono">LOCATION</h1>
-          <p>1234 Street Name, City, State, 12345</p>
-          <p>crosfit@fit.com</p>
+          <h1 className="text-center font-bold text-white font-mono md:text-start ">LOCATION</h1>
+          <p className="pb-1">1234 Street Name, City, State, 12345</p>
+          <p className="pb-1">crosfit@fit.com</p>
           <p>+1 234 567 890</p>
         </div>
 
         <div className="py-5">
-          <h1 className="flex font-bold text-white font-mono">
-            START NOW
+          <div className="flex items-center justify-center md:justify-start">
+            <h1 className=" font-bold text-white font-mono ">
+              START NOW
+            </h1>
             <Image
               src="cabeica-p-baixo-seta.svg"
               alt="Arrow Down"
@@ -52,11 +55,10 @@ const Footer = () => {
               height={20}
               className="ml-2"
             />
-          </h1>
+          </div>
 
-
-          <ul className="flex gap-4">
-            <li>
+          <ul className="flex gap-4 align-items-center justify-center md:justify-start pt-3">
+            <li className="flex gap-4 align-items-center justify-center">
               <Image
                 src="tiktok.svg"
                 alt="tiktok"
@@ -64,7 +66,7 @@ const Footer = () => {
                 height={20}
               />
             </li>
-            <li>
+            <li className="flex gap-4 align-items-center justify-center">
               <Image
                 src="youtube.svg"
                 alt="youtube"
@@ -72,15 +74,15 @@ const Footer = () => {
                 height={20}
               />
             </li>
-            <li>
+            <li className="flex align-items-center justify-center">
               <Image
                 src="facebook.svg"
                 alt="facebook"
-                width={20}
-                height={20}
+                width={13}
+                height={13}
               />
             </li>
-            <li>
+            <li className="flex gap-4 align-items-center justify-center">
               <Image
                 src="instagram.svg"
                 alt="instagram"
@@ -88,9 +90,9 @@ const Footer = () => {
                 height={20}
               />
             </li>
-            <li>
+            <li className="flex gap-4 align-items-center justify-center">
               <Image
-                src="twitter.svg"
+                src="twiter.svg"
                 alt="twitter"
                 width={20}
                 height={20}
