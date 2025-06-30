d44e5abecde28f2530931646c4e37b6467032e3d
RebecaTI <mebuffbeka@gmail.com>
2025-06-30 10:38:27 -0300

    refactor: uncomment components in Home for improved page structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,19 @@ import JoinUs from "@/components/JoinUs";
 import PowerQuote from "@/components/PowerQuote";
 import { HealthGoal } from "@/components/HealthGoal";
 import HeroSection from "@/components/hero";
-// import FitFamilySection from "@/components/fitFamily";
+import FitFamilySection from "@/components/fitFamily";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div className="  background-page">
       <HeroSection />
-      {/* <PowerQuote /> */}
-      {/* <HealthGoal /> */}
-      {/* <JoinUs /> */}
+      <PowerQuote />
+      <HealthGoal />
+      <JoinUs />
       {/* <FindUs /> */}
-      {/* <FitFamilySection /> */}
-      {/* <Footer /> */}
+      <FitFamilySection />
+      <Footer />
     </div>
   );
 }
